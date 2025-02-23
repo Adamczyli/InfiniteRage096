6fc4dc528714b05fd7477ea5211fdf8f2dc14fbe
kidki <kidki@DESKTOP-2N1U5MU>
2025-02-23 22:23:38 +0100

    Dodaj pliki projektów.

---
+++ InfiniteRage096/Class1.cs
@@ -0,0 +1,51 @@
+﻿using Exiled.API.Features;
+using Exiled.CreditTags;
+using Exiled.Events.EventArgs.Scp096; 
+using Scp096 = Exiled.Events.Handlers.Scp096;
+
+namespace InfinityRage
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Infinity rage096";
+        public override string Author => ".Adamczyli <3";
+        public override string Prefix => "infinityrage";
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            Scp096.AddingTarget += OnAddingTarget;
+            Scp096.CalmingDown += OnRagingEnded; 
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            Scp096.AddingTarget -= OnAddingTarget;
+            Scp096.CalmingDown -= OnRagingEnded;
+        }
+
+        private void OnAddingTarget(AddingTargetEventArgs ev)
+        {
+          
+        }
+
+        private void OnRagingEnded(CalmingDownEventArgs ev)
+        {
+            if (ev.Scp096.Targets.Count > 0)
+            {
+
+                ev.IsAllowed = false;
+                Log.Debug($"Rage trwa dalej, ponieważ pozostało <color=#FF0101>{ev.Scp096.Targets.Count}</color> targetów do zabicia!");
+                ev.Player.ShowHint($"Rage trwa dalej, ponieważ pozostało <color=#FF0101>{ev.Scp096.Targets.Count}</color> targetów.", 1);
+            }
+            if (ev.Scp096.Targets.Count < 0)
+            {
+                ev.IsAllowed = true;
+                Log.Debug($"⚠️Rage się zakończył, ponieważ nie masz żadnych targetów.⚠️");
+            }
+        }
+    }
+}
